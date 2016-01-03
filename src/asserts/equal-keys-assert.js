9a332519b824f265167ec786f6a3ed4f6af05c3b
Rui Marinho <ruipmarinho@gmail.com>
2016-01-03 20:32:27 +0000

    Handle empty objects in `EqualKeys`
    Closes #34.

--- src/asserts/equal-keys-assert.js
+++ src/asserts/equal-keys-assert.js
@@ -4,18 +4,22 @@
  */
 
 import { Violation } from 'validator.js';
-import { difference, isPlainObject } from 'lodash';
+import { difference, intersection, isArray, isPlainObject } from 'lodash';
 
 /**
  * Export `EqualKeysAssert`.
  */
 
-export default function equalKeysAssert(keys) {
+export default function equalKeysAssert(...keys) {
   /**
    * Class name.
    */
 
   this.__class__ = 'EqualKeys';
+
+  if (keys.length === 1 && isArray(keys[0])) {
+    keys = keys[0];
+  }
 
   /**
    * Keys.
@@ -32,10 +36,16 @@ export default function equalKeysAssert(keys) {
       throw new Violation(this, value, { value: 'must_be_a_plain_object' });
     }
 
-    const diff = difference(Object.keys(value), this.keys);
+    const keys = Object.keys(value);
 
-    if (diff.length > 0) {
-      throw new Violation(this, value, { difference: diff });
+    if (keys.length === 0 && this.keys.length > 0 || this.keys.length > keys.length) {
+      throw new Violation(this, value, { difference: difference(this.keys, keys) });
+    }
+
+    const intersects = intersection(this.keys, keys);
+
+    if (keys.length > this.keys.length || intersects.length !== keys.length) {
+      throw new Violation(this, value, { difference: difference(keys, this.keys) });
     }
 
     return true;
--- test/asserts/equal-keys-assert_test.js
+++ test/asserts/equal-keys-assert_test.js
@@ -35,7 +35,7 @@ describe('EqualKeysAssert', () => {
     });
   });
 
-  it('should throw an error if an object does not have the expected keys', () => {
+  it('should throw an error if the object does not have the expected keys', () => {
     try {
       new Assert().EqualKeys(['foo']).validate({ bar: 'biz', foo: 'qux' });
 
@@ -45,9 +45,49 @@ describe('EqualKeysAssert', () => {
     }
   });
 
+  it('should throw an error if the object is empty', () => {
+    try {
+      new Assert().EqualKeys(['foo']).validate({});
+
+      should.fail();
+    } catch (e) {
+      e.show().violation.difference.should.eql(['foo']);
+    }
+  });
+
+  it('should allow `keys` to be `undefined`', () => {
+    try {
+      new Assert().EqualKeys().validate({ foo: 'oof' });
+
+      should.fail();
+    } catch (e) {
+      e.show().violation.difference.should.eql(['foo']);
+    }
+  });
+
+  it('should allow `keys` to be defined as multiple arguments', () => {
+    try {
+      new Assert().EqualKeys('foo', 'bar').validate({ foo: 'oof' });
+
+      should.fail();
+    } catch (e) {
+      e.show().violation.difference.should.eql(['bar']);
+    }
+  });
+
+  it('should allow `keys` to be defined as a single string argument', () => {
+    try {
+      new Assert().EqualKeys('bar').validate({ foo: 'oof' });
+
+      should.fail();
+    } catch (e) {
+      e.show().violation.difference.should.eql(['foo']);
+    }
+  });
+
   it('should expose `assert` equal to `EqualKeys`', () => {
     try {
-      new Assert().EqualKeys().validate(123);
+      new Assert().EqualKeys(['foo']).validate(123);
 
       should.fail();
     } catch (e) {
@@ -55,7 +95,7 @@ describe('EqualKeysAssert', () => {
     }
   });
 
-  it('should expose `difference` on the violation', () => {
+  it('should expose `difference` on the violation if object has extra keys', () => {
     try {
       new Assert().EqualKeys(['foo']).validate({ bar: 'biz', foo: 'qux' });
 
@@ -63,6 +103,20 @@ describe('EqualKeysAssert', () => {
     } catch (e) {
       e.show().violation.difference.should.eql(['bar']);
     }
+  });
+
+  it('should expose `difference` on the violation if object has missing keys', () => {
+    try {
+      new Assert().EqualKeys(['foo', 'biz']).validate({ foo: 'qux' });
+
+      should.fail();
+    } catch (e) {
+      e.show().violation.difference.should.eql(['biz']);
+    }
+  });
+
+  it('should accept an empty object with no keys expected', () => {
+    new Assert().EqualKeys().validate({});
   });
 
   it('should accept an object with expected keys', () => {
