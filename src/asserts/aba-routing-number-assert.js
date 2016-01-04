3e4365581aa62b00168bf77a0d73369c7e344fe7
João Barbosa <joao@uphold.com>
2016-01-04 16:43:15 +0000

    Add ABA Routing Number assert

---
+++ src/asserts/aba-routing-number-assert.js
@@ -0,0 +1,38 @@
+
+/**
+ * Module dependencies.
+ */
+
+import abaValidator from 'abavalidator';
+import { Validator, Violation } from 'validator.js';
+
+/**
+ * Export `AbaRoutingNumberAssert`.
+ */
+
+export default function abaRoutingNumberAssert() {
+  /**
+   * Class name.
+   */
+
+  this.__class__ = 'AbaRoutingNumber';
+
+  /**
+   * Validation algorithm.
+   */
+
+  this.validate = (value) => {
+    if (typeof value !== 'string') {
+      // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+      throw new Violation(this, value, { value: Validator.errorCode.must_be_a_string });
+    }
+
+    if (!abaValidator.validate(value)) {
+      throw new Violation(this, value);
+    }
+
+    return true;
+  };
+
+  return this;
+}
---
+++ test/asserts/aba-routing-number-assert_test.js
@@ -0,0 +1,60 @@
+
+/**
+ * Module dependencies.
+ */
+
+import { Assert as BaseAssert, Violation } from 'validator.js';
+import AbaRoutingNumberAssert from '../../src/asserts/aba-routing-number-assert';
+import should from 'should';
+
+/**
+ * Extend `Assert` with `AbaRoutingNumberAssert`.
+ */
+
+const Assert = BaseAssert.extend({
+  AbaRoutingNumber: AbaRoutingNumberAssert
+});
+
+/**
+ * Test `RoutingNumberAssert`.
+ */
+
+describe('AbaRoutingNumberAssert', () => {
+  it('should throw an error if the input value is not a string', () => {
+    [{}, []].forEach((choice) => {
+      try {
+        new Assert().AbaRoutingNumber().validate(choice);
+
+        should.fail();
+      } catch (e) {
+        e.should.be.instanceOf(Violation);
+        e.violation.value.should.equal('must_be_a_string');
+      }
+    });
+  });
+
+  it('should throw an error if the input value is not a valid ABA routing number', () => {
+    try {
+      new Assert().AbaRoutingNumber().validate('foobar');
+
+      should.fail();
+    } catch (e) {
+      e.should.be.instanceOf(Violation);
+      e.show().value.should.equal('foobar');
+    }
+  });
+
+  it('should expose `assert` equal to `AbaRoutingNumber`', () => {
+    try {
+      new Assert().AbaRoutingNumber().validate(123);
+
+      should.fail();
+    } catch (e) {
+      e.show().assert.should.equal('AbaRoutingNumber');
+    }
+  });
+
+  it('should accept a valid ABA routing number', () => {
+    new Assert().AbaRoutingNumber().validate('123123123');
+  });
+});
