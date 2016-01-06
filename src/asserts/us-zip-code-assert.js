d40884069cea0544f9a14de4589457c0295fe968
João Barbosa <joao@uphold.com>
2016-01-06 10:27:53 +0000

    Add US Zip Code assert

---
+++ src/asserts/us-zip-code-assert.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+
+import { Validator, Violation } from 'validator.js';
+
+/**
+ * US zip code regular expression.
+ */
+
+const regexp = new RegExp(/^\d{5}(?:[- ]?\d{4})?$/);
+
+/**
+ * Export `UsZipCodeAssert`.
+ */
+
+export default function usZipCodeAssert() {
+  /**
+   * Class name.
+   */
+
+  this.__class__ = 'UsZipCode';
+
+  /**
+   * Validation algorithm.
+   */
+
+  this.validate = function(value) {
+    if (typeof value !== 'string') {
+      // jscs: disable requireCamelCaseOrUpperCaseIdentifiers
+      throw new Violation(this, value, { value: Validator.errorCode.must_be_a_string });
+    }
+
+    if (!regexp.test(value)) {
+      throw new Violation(this, value);
+    }
+
+    return true;
+  };
+
+  return this;
+}
---
+++ test/asserts/us-zip-code-assert_test.js
@@ -0,0 +1,64 @@
+
+/**
+ * Module dependencies.
+ */
+
+import { Assert as BaseAssert, Validator, Violation } from 'validator.js';
+import UsZipCodeAssert from '../../src/asserts/us-zip-code-assert';
+import should from 'should';
+
+/**
+ * Extend `Assert` with `UsZipCodeAssert`.
+ */
+
+const Assert = BaseAssert.extend({
+  UsZipCode: UsZipCodeAssert
+});
+
+/**
+ * Test `UsZipCodeAssert`.
+ */
+
+describe('UsZipCodeAssert', () => {
+  it('should throw an error if the input value is not a string', () => {
+    const choices = [[], {}, 123];
+
+    choices.forEach((choice) => {
+      try {
+        new Assert().UsZipCode().validate(choice);
+
+        should.fail();
+      } catch (e) {
+        e.should.be.instanceOf(Violation);
+
+        // jscs: disable requireCamelCaseOrUpperCaseIdentifiers
+        e.violation.value.should.equal(Validator.errorCode.must_be_a_string);
+      }
+    });
+  });
+
+  it('should throw an error if `value` is invalid', () => {
+    const choices = ['#', '12345--1234', '12345\t1234', '1234-12345', '1234'];
+
+    choices.forEach((choice) => {
+      try {
+        new Assert().UsZipCode().validate(choice);
+
+        should.fail();
+      } catch (e) {
+        e.should.be.instanceOf(Violation);
+        e.show().assert.should.equal('UsZipCode');
+      }
+    });
+  });
+
+  it('should accept 5-digit zip codes', () => {
+    new Assert().UsZipCode().validate('12345');
+  });
+
+  it('should accept 9-digit zip codes', () => {
+    ['12345-1234', '12345 1234', '123456789'].forEach((choice) => {
+      new Assert().UsZipCode().validate(choice);
+    });
+  });
+});
