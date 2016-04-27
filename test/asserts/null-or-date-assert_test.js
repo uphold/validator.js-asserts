111755853aa7adc28cad4735ce1d6fb44d90974c
Joao Madeiras <madeiras@uphold.com>
2016-04-27 15:48:47 +0100

    Add support for numeric timestamps on `NullOrDateAssert`

--- test/asserts/null-or-date-assert_test.js
+++ test/asserts/null-or-date-assert_test.js
@@ -20,8 +20,8 @@ const Assert = BaseAssert.extend({
  */
 
 describe('NullOrDateAssert', () => {
-  it('should throw an error if the input value is not a `null` or a date', () => {
-    const choices = [[], {}, 123];
+  it('should throw an error if the input value is not a `null` or a date or a number', () => {
+    const choices = [[], {}];
 
     choices.forEach(choice => {
       try {
@@ -30,7 +30,7 @@ describe('NullOrDateAssert', () => {
         should.fail();
       } catch (e) {
         e.should.be.instanceOf(Violation);
-        e.violation.value.should.equal('must_be_null_or_a_date');
+        e.violation.value.should.equal('must_be_null_or_a_date_or_a_number');
       }
     });
   });
@@ -43,6 +43,28 @@ describe('NullOrDateAssert', () => {
     } catch (e) {
       e.should.be.instanceOf(Violation);
       e.show().value.should.equal('2015-99-01');
+    }
+  });
+
+  it('should throw an error if the input value is an invalid timestamp', () => {
+    try {
+      new Assert().NullOrDate().validate(-Number.MAX_VALUE);
+
+      should.fail();
+    } catch (e) {
+      e.should.be.instanceOf(Violation);
+      e.show().assert.should.equal('NullOrDate');
+    }
+  });
+
+  it('should throw an error if value does not pass strict validation', () => {
+    try {
+      new Assert().NullOrDate({ format: 'YYYY-MM-DD' }).validate('2000.12.30');
+
+      should.fail();
+    } catch (e) {
+      e.should.be.instanceOf(Violation);
+      e.show().assert.should.equal('NullOrDate');
     }
   });
 
@@ -64,7 +86,19 @@ describe('NullOrDateAssert', () => {
     new Assert().NullOrDate().validate(new Date());
   });
 
+  it('should accept a correctly formatted date', () => {
+    new Assert().NullOrDate({ format: 'YYYY' }).validate('2000');
+  });
+
   it('should accept a string', () => {
     new Assert().NullOrDate().validate('2014-10-16');
+  });
+
+  it('should accept an ISO-8601 string date', () => {
+    new Assert().NullOrDate().validate('2016-04-23T00:51:18.570Z');
+  });
+
+  it('should accept a numeric timestamp', () => {
+    new Assert().NullOrDate().validate(Date.now());
   });
 });
